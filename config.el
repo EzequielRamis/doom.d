91923e2e242bd1575e81e007ea1b340ebdc7a8d7
Ezequiel Ramis <ezequielramis.hello@gmail.com>
2022-05-20 22:24:17 -0300

    emoji

--- config.el
+++ config.el
@@ -20,6 +20,7 @@
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 (setq doom-font (font-spec :family "SFMono Nerd Font" :size 18))
+(setq doom-emoji-fallback-font-families '("Twitter Color Emoji"))
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
